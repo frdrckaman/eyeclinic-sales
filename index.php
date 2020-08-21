e2cc1daa58e2f6a206783c480c10ca62a783de3f
frdrck <frdrckaman@gmail.com>
2020-08-21 06:25:13 +0300

    template modification

--- index.php
+++ index.php
@@ -1,1 +1,81 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Login - OnaEyeCare</title>
+    <?php include 'head.php'?>
+</head>
+<body>
+
+<div class="loginBlock" id="login" style="display: block;">
+    <h1>Welcom. Please Sign In</h1>
+    <div class="dr"><span></span></div>
+    <div class="loginForm">
+        <form class="form-horizontal" method="post" id="validation">
+            <div class="form-group">
+                <div class="input-group">
+                    <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
+                    <input type="text" id="inputEmail" placeholder="Email" class="form-control validate[required]"/>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="input-group">
+                    <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+                    <input type="password" id="inputPassword" placeholder="Password" class="form-control validate[required]"/>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="form-group" style="margin-top: 5px;">
+                        <label class="checkbox"><input type="checkbox"> Remember me</label>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <button type="submit" class="btn btn-default btn-block">Sign in</button>
+                </div>
+            </div>
+        </form>
+        <div class="dr"><span></span></div>
+        <div class="controls">
+            <div class="row">
+                <div class="col-md-6">
+                    <button class="btn btn-link btn-block" onClick="loginBlock('#forgot');">Forgot password?</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="loginBlock" id="forgot">
+    <h1>Forgot your password?</h1>
+    <div class="dr"><span></span></div>
+    <div class="loginForm">
+        <form class="form-horizontal" action="http://aqvatarius.com/themes/aquarius/index.html" method="POST">
+            <p>This form help you return your password. Please, enter your password, and send request</p>
+            <div class="form-group">
+                <div class="input-group">
+                    <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
+                    <input type="text" placeholder="Your email" class="form-control"/>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6"></div>
+                <div class="col-md-6">
+                    <button type="submit" class="btn btn-default btn-block">Send request</button>
+                </div>
+            </div>
+        </form>
+        <div class="dr"><span></span></div>
+        <div class="controls">
+            <div class="row">
+                <div class="col-md-12">
+                    <button class="btn btn-link" onClick="loginBlock('#login');">&laquo; Back</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+
+</html>
---
+++ menu.php
@@ -0,0 +1,116 @@
+<div class="menu">
+
+    <div class="breadLine">
+        <div class="arrow"></div>
+        <div class="adminControl active">
+            Hi, Aqvatarius
+        </div>
+    </div>
+
+    <div class="admin">
+        <div class="image">
+            <img src="img/users/aqvatarius.jpg" class="img-thumbnail"/>
+        </div>
+        <ul class="control">
+            <li><span class="glyphicon glyphicon-comment"></span> <a href="#">Messages</a> <a href="#" class="caption red">12</a></li>
+            <li><span class="glyphicon glyphicon-cog"></span> <a href="#">Settings</a></li>
+            <li><span class="glyphicon glyphicon-share-alt"></span> <a href="#">Logout</a></li>
+        </ul>
+        <div class="info">
+            <span>Welcom back! Your last visit: 24.10.2012 in 19:55</span>
+        </div>
+    </div>
+
+    <ul class="navigation">
+        <li class="active">
+            <a href="dashboard.php">
+                <span class="isw-grid"></span><span class="text">Dashboard</span>
+            </a>
+        </li>
+        <li class="active">
+            <a href="dashboard.php">
+                <span class="isw-list"></span><span class="text">Add Stock</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <span class="isw-archive"></span><span class="text">Assign Stock</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <span class="isw-settings"></span><span class="text">Stock Management</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <span class="isw-user"></span><span class="text">Add staff</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <span class="isw-users"></span><span class="text">Manage staff</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="#">
+                <span class="isw-graph"></span><span class="text">Add Clinic Branch</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <span class="isw-text_document"></span><span class="text">Manage Clinic Branch</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="#">
+                <span class="isw-zoom"></span><span class="text">Reports</span>
+            </a>
+        </li>
+
+    </ul>
+
+    <div class="dr"><span></span></div>
+
+    <div class="widget-fluid">
+        <div id="menuDatepicker"></div>
+    </div>
+
+    <div class="dr"><span></span></div>
+
+    <div class="widget">
+
+        <div class="input-group">
+            <input id="appendedInputButton" class="form-control" type="text">
+            <div class="input-group-btn">
+                <button class="btn btn-default" type="button">Search</button>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="dr"><span></span></div>
+
+    <div class="widget-fluid">
+
+        <div class="wBlock clearfix">
+            <div class="dSpace">
+                <h3>Last visits</h3>
+                <span class="number">6,302</span>
+                <span>5,774 <b>unique</b></span>
+                <span>3,512 <b>returning</b></span>
+            </div>
+            <div class="rSpace">
+                <h3>Today</h3>
+                <span class="mChartBar" sparkType="bar" sparkBarColor="white"><!--240,234,150,290,310,240,210,400,320,198,250,222,111,240,221,340,250,190--></span>
+                <span>&nbsp;</span>
+                <span>65% <b>New</b></span>
+                <span>35% <b>Returning</b></span>
+            </div>
+        </div>
+
+    </div>
+
+</div>
